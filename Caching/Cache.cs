7abcbb0b8bbf1916ce1127fd6ff36d4e31e46da9
Mintaka <spaceisperfect@gmail.com>
2019-02-12 22:26:15 +0300

    Add comments.

--- Caching/Cache.cs
+++ Caching/Cache.cs
@@ -3,25 +3,45 @@
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Provides methods to cache objects for limited amount of time.
+    /// </summary>
     public class Cache
     {
-        private Dictionary<string, CacheItem> cache;
+        private readonly Dictionary<string, CacheItem> cache;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cache"/> class. 
+        /// </summary>
         public Cache()
         {
             this.cache = new Dictionary<string, CacheItem>();
         }
 
+        /// <summary>
+        /// Adds object to the cache.
+        /// </summary>
+        /// <param name="key">A key.</param>
+        /// <param name="obj">An object to store.</param>
+        /// <param name="timeInSeconds">Storage time in seconds.</param>
+        /// <exception cref="InvalidOperationException">Thrown when object with such key already exists.</exception>
         public void AddItem(string key, object obj, int timeInSeconds)
         {
             if (this.cache.ContainsKey(key))
             {
-                throw new InvalidOperationException("Object with this name already exists.");
+                throw new InvalidOperationException("Object with this key already exists.");
             }
 
             this.cache.Add(key, new CacheItem(obj, timeInSeconds));
         }
 
+        /// <summary>
+        /// Retrieves an item from cache.
+        /// </summary>
+        /// <param name="key">A key.</param>
+        /// <exception cref="KeyNotFoundException">Thrown when key doesn't exist.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the object expired.</exception>
+        /// <returns>Object represented by the key.</returns>
         public object GetItem(string key)
         {
             if (!this.cache.ContainsKey(key))
